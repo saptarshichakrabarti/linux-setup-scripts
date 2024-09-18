910436a78b52a2ccc3e44a7753c9ac9d3929cbb0
Saptarshi Chakrabarti <13855778+saptarshichakrabarti@users.noreply.github.com>
2024-09-18 21:26:35 +0200

    Update icon_install.sh

--- scripts/icon_install.sh
+++ scripts/icon_install.sh
@@ -23,7 +23,7 @@ if [ -z "$EXTRACTED_DIR" ]; then
 fi
 
 # Create .icons folder in the home directory if it doesn't exist
-ICONS_DIR="$HOME/.icons"
+ICONS_DIR="$HOME/.icons/GruvboxPlus"
 mkdir -p $ICONS_DIR
 
 # Move extracted contents to ~/.icons folder
